9af89473e9fe15e62bbc9e3369ea0c39b5e616c0
warlyn <warlyn200@gmail.com>
2025-03-16 23:04:28 -0400

    Add default admin user seeder
    This change seeds the database with a default admin user for testing or initial setup. It includes predefined credentials such as name, email, and username for easier access.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@portal.com',
+            'username' => 'admin',
+        ]);
     }
 }
