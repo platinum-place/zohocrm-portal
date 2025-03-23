2e0a0fc0740b537cee9f75630b665ea29aacc10d
warlyn <warlyn200@gmail.com>
2025-03-23 19:19:22 -0400

    Add layout component and integrate it with login view
    Created a reusable layout template in `components/layout.php` to standardize page structure. Updated the login view to extend the new layout, improving maintainability and consistency. Included basic styling and flash message display for user feedback.

---
+++ app/Views/components/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>IT - Insurance Tech</title>
+    <link rel="icon" type="image/png" href="<?= base_url('images/favicon.ico') ?>">
+    <link href="<?= base_url('css/styles.css') ?>" rel="stylesheet"/>
+    <?= $this->renderSection('css') ?>
+</head>
+
+<body>
+<?= $this->renderSection('content') ?>
+
+<?= $this->renderSection('js') ?>
+</body>
+
+</html>
