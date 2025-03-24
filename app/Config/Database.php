5cce0173c3b3fbb151fc133cf97c1c5e47f4dc20
warlyn <warlyn200@gmail.com>
2025-03-23 21:46:55 -0400

    Switch default database to SQLite.
    Updated the

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,12 +31,12 @@ class Database extends Config
      * @var array
      */
     public $default = [
-        'DSN'      => '',
-        'hostname' => 'localhost',
+        'DSN'      => 'sqlite:' . WRITEPATH . 'database/database.sqlite',
+        'hostname' => '',
         'username' => '',
         'password' => '',
-        'database' => '',
-        'DBDriver' => 'MySQLi',
+        'database' => WRITEPATH . 'database/database.sqlite',
+        'DBDriver' => 'SQLite3',
         'DBPrefix' => '',
         'pConnect' => false,
         'DBDebug'  => (ENVIRONMENT !== 'production'),
@@ -47,7 +47,25 @@ class Database extends Config
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port'     => 3306,
+        'port'     => 0,
+
+//        'DSN'      => '',
+//        'hostname' => 'localhost',
+//        'username' => '',
+//        'password' => '',
+//        'database' => '',
+//        'DBDriver' => 'MySQLi',
+//        'DBPrefix' => '',
+//        'pConnect' => false,
+//        'DBDebug'  => (ENVIRONMENT !== 'production'),
+//        'charset'  => 'utf8',
+//        'DBCollat' => 'utf8_general_ci',
+//        'swapPre'  => '',
+//        'encrypt'  => false,
+//        'compress' => false,
+//        'strictOn' => false,
+//        'failover' => [],
+//        'port'     => 3306,
     ];
 
     /**
