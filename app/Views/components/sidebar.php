5a1d3cdbeb8b02514649770ef04ba892c90a9511
warlyn <warlyn200@gmail.com>
2025-03-23 22:47:30 -0400

    Add sidebar component for application layout
    Implement a new sidebar component in the application to enhance navigation. The sidebar includes sections for dashboard, quotations, and emissions, along with user information in the footer. This addition lays the groundwork for a consistent and user-friendly interface.

---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,35 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="text-center">
+                    <br />
+                    <img src="<?= base_url('images/logo.png') ?>" alt="Logo IT" width="150" height="150">
+                </div>
+                <div class="sb-sidenav-menu-heading">&nbsp;</div>
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Panel de Control
+                </a>
+                <div class="sb-sidenav-menu-heading">Cotizaciones</div>
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Cotizar
+                </a>
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                    Buscar Cotización
+                </a>
+                <div class="sb-sidenav-menu-heading">Emisiones</div>
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Buscar Emisión
+                </a>
+            </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Ingresaste como:</div>
+            Usuario de ejemplo
+        </div>
+    </nav>
+</div>
