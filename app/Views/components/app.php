4ae9a2958de12c40be20a734fc0d60f2e4b897fe
warlyn <warlyn200@gmail.com>
2025-03-23 22:21:32 -0400

    Add layout components for app structure and navigation
    Introduces `app.php` for the main HTML structure, a new `navbar.php` component, and updates `layout.php` to include shared elements like footer and FontAwesome integration. These changes improve code reusability and maintainability for the application's UI.

--- app/Views/components/app.php
+++ app/Views/components/app.php
@@ -10,11 +10,13 @@
     <title>IT - Insurance Tech</title>
     <link rel="icon" type="image/png" href="<?= base_url('images/favicon.ico') ?>">
     <link href="<?= base_url('css/styles.css') ?>" rel="stylesheet"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.min.css">
     <?= $this->renderSection('css') ?>
 </head>
 
 <body>
+
+<?= $this->include('components/navbar') ?>
 
 <?= $this->renderSection('content') ?>
 
---
+++ app/Views/components/navbar.php
@@ -0,0 +1,18 @@
+<nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #004583">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="#"><?= session('username') ?></a>
+    <!-- Navbar Search-->
+    <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        &nbsp;
+    </div>
+    <!-- Navbar-->
+    <form method="POST" action="<?= site_url('login') ?>" class="ms-auto ms-md-0 me-3 me-lg-4">
+        <?= csrf_field() ?>
+
+        <input type="hidden" name="_method" value="PUT">
+
+        <button type="submit" class="btn btn-link p-0">
+            <i class="fas fa-sign-out-alt fa-fw"></i>
+        </button>
+    </form>
+</nav>
