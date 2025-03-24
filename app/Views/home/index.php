dbdbadd64149f7138ea539b365cd1a5c09e9827b
warlyn <warlyn200@gmail.com>
2025-03-23 22:47:35 -0400

    Add welcome message to home view
    Introduced a heading and a success alert box to the home view for improved user onboarding. The alert includes a welcome message and a brief description of the dashboard's functionalities.

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -2,8 +2,14 @@
 
 <?= $this->section('content') ?>
 
+<h1 class="mt-4">Panel de Control</h1>
+<hr>
 
-
-
+<div class="alert alert-success" role="alert">
+    <h3 class="alert-heading">¡Bienvenido al Insurance Tech de Grupo Nobe!</h3>
+    <p>
+        Desde su panel de control podrás ver la información necesaria para manejar sus pólizas y cotizaciones.
+    </p>
+</div>
 
 <?= $this->endSection() ?>
