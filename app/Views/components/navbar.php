60334d05ba66c269a1c8d2ba0acf8dc6a004f831
warlyn <warlyn200@gmail.com>
2025-03-24 20:57:54 -0400

    Refactor navbar and sidebar for improved functionality.
    Updated the navbar to include a dropdown menu with a logout option and enhanced aesthetics. Refactored the sidebar to include dynamic links, admin-specific tools, and session-based user display. These changes improve navigation and user experience.

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,18 +1,31 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #004583">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="#"><?= session('company_name') ?></a>
+    <a class="navbar-brand ps-3" href="<?= site_url() ?>"><?= session('name') ?></a>
+    <!-- Sidebar Toggle-->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+                class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
     <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         &nbsp;
     </div>
     <!-- Navbar-->
-    <form method="POST" action="<?= site_url('login') ?>" class="ms-auto ms-md-0 me-3 me-lg-4">
-        <?= csrf_field() ?>
+    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+               aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li>
+                    <form method="POST" action="<?= site_url('logout') ?>">
+                        <?= csrf_field() ?>
 
-        <input type="hidden" name="_method" value="PUT">
+                        <input type="hidden" name="_method" value="PUT">
 
-        <button type="submit" class="btn btn-link p-0">
-            <i class="fas fa-sign-out-alt fa-fw"></i>
-        </button>
-    </form>
+                        <button type="submit" class="dropdown-item">
+                            Cerrar sesión
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </nav>
