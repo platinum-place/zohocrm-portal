153cc1b0ac8f56bedc47b28dceeda0a2d1db795d
warlyn <warlyn200@gmail.com>
2025-03-24 20:58:03 -0400

    Enhance dashboard with real-time quote count integration
    Added functionality to display the count of quotes issued this month on the dashboard, sourced dynamically from the Zoho API. Refactored the `index` method to incorporate ZohoClient for improved data handling. Included session-based filtering for personalized quote data retrieval.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,57 @@
 
 namespace App\Controllers;
 
+
+use App\Libraries\Emisiones;
+use App\Libraries\Zoho;
+use App\Libraries\ZohoClient;
+
 class Home extends BaseController
 {
+    protected $zoho;
+
+    public function __construct()
+    {
+        $this->zoho = new ZohoClient();
+    }
+
     public function index()
     {
-        return view('home/index');
+        $criterio = "Quote_Stage:starts_with:E";
+
+        if (session('zoho_company_id')) {
+            $criterio = "((Account_Name:equals:" . session('zoho_company_id') . ") and (Contact_Name:equals:" . session('zoho_id') . ") and ($criterio))";
+        }
+
+        $quotes_count = $this->zoho->recordCountInAModule('Quotes', $criterio);
+
+        return view('home/index', [
+            'quotes_count' => $quotes_count['count'],
+            'quotes_list' => array(),
+        ]);
     }
+
+//    public function index(): string
+//    {
+//        $libreria = new \App\Libraries\Cotizaciones();
+//        $emisiones = $libreria->lista_emisiones();
+//
+//        $lista = array();
+//        $polizas = 0;
+//
+//        foreach ((array)$emisiones as $emision) {
+//            //filtrar por  mes y año actual
+//            if (date("Y-m", strtotime($emision->getFieldValue("Vigencia_desde"))) == date("Y-m")) {
+//                $lista[] = $emision->getFieldValue('Coberturas')->getLookupLabel();
+//                $polizas++;
+//            }
+//        }
+//
+//        return view('index', [
+//            "titulo" => "Panel de Control",
+//            "lista" => array_count_values($lista),
+//            "polizas" => $polizas,
+//            "cotizaciones" => $emisiones,
+//        ]);
+//    }
 }
--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -5,11 +5,50 @@
 <h1 class="mt-4">Panel de Control</h1>
 <hr>
 
+<?php if (session()->getFlashdata('alert')) : ?>
+    <div class="alert alert-success" role="alert">
+        <?= session()->getFlashdata('alert') ?>
+    </div>
+<?php endif ?>
+
 <div class="alert alert-success" role="alert">
     <h3 class="alert-heading">¡Bienvenido al Insurance Tech de Grupo Nobe!</h3>
     <p>
         Desde su panel de control podrás ver la información necesaria para manejar sus pólizas y cotizaciones.
     </p>
 </div>
+
+<div class="row">
+    <div class="col-xl-3 col-md-4">
+        <div class="card bg-success text-white mb-4">
+            <div class="card-body">
+                Pólizas emitidas este mes<br>
+                <?= $quotes_count ?? 0 ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--<div class="card mb-4">-->
+<!--    <div class="card-body">-->
+<!--        <table class="table">-->
+<!--            <thead>-->
+<!--            <tr>-->
+<!--                <th>Aseguradora</th>-->
+<!--                <th>Cantidad</th>-->
+<!--            </tr>-->
+<!--            </thead>-->
+<!---->
+<!--            <tbody>-->
+<!--            --><?php //foreach ($lista as $aseguradora => $cantidad) : ?>
+<!--                <tr>-->
+<!--                    <td>--><?php //= $aseguradora ?><!--</td>-->
+<!--                    <td>--><?php //= $cantidad ?><!--</td>-->
+<!--                </tr>-->
+<!--            --><?php //endforeach ?>
+<!--            </tbody>-->
+<!--        </table>-->
+<!--    </div>-->
+<!--</div>-->
 
 <?= $this->endSection() ?>
