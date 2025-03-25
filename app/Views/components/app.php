96173ee26996a90c6dbd134347736b50c06aa3c8
warlyn <warlyn200@gmail.com>
2025-03-24 20:57:32 -0400

    Update app layout with new styles, scripts, and footer
    Added new stylesheet and scripts for improved functionality, including Simple-DataTables, Font Awesome JS, and SweetAlert2. Updated the body tag with a class for navigation styling and introduced a footer section for copyright display. Enhanced the structure to support better user experience and maintainability.

--- app/Views/components/app.php
+++ app/Views/components/app.php
@@ -10,11 +10,13 @@
     <title>IT - Insurance Tech</title>
     <link rel="icon" type="image/png" href="<?= base_url('images/favicon.ico') ?>">
     <link href="<?= base_url('css/styles.css') ?>" rel="stylesheet"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
+            crossorigin="anonymous"></script>
     <?= $this->renderSection('css') ?>
 </head>
 
-<body>
+<body class="sb-nav-fixed">
 
 <?= $this->include('components/navbar') ?>
 
@@ -27,9 +29,26 @@
                 <?= $this->renderSection('content') ?>
             </div>
         </main>
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; <?= date('Y') ?></div>
+                </div>
+            </div>
+        </footer>
     </div>
 </div>
 
+<?= $this->renderSection('content') ?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+<script src="<?= base_url('js/scripts.js') ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="<?= base_url('js/datatables-simple-demo.js') ?>"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?= $this->renderSection('js') ?>
 </body>
 
