f039cb56e2db4453708591723cd5ca15457ad259
warlyn <warlyn200@gmail.com>
2025-03-24 20:58:10 -0400

    Make `generaAccessToken` protected and add record count method
    Refactored `generaAccessToken` to be a protected method for better encapsulation. Added `recordCountInAModule` method to fetch record count for a module with given criteria, utilizing Zoho API. Included error handling and updated token generation logic.

--- app/Libraries/ZohoClient.php
+++ app/Libraries/ZohoClient.php
@@ -42,14 +42,38 @@ class ZohoClient
     /**
      * @throws Exception
      */
-    public function generaAccessToken()
+    protected function generaAccessToken()
     {
         $response = $this->client->request('post', $this->config->url_token, [
             'form_params' => [
                 'grant_type' => 'refresh_token',
                 'client_id' => $this->config->client_id,
                 'client_secret' => $this->config->client_secret,
-                'redirect_uri' => $this->config->redirect_uri,
+                'refresh_token' => $this->config->refresh_token,
+            ],
+        ]);
+
+        $statusCode = $response->getStatusCode();
+        if ($statusCode < 200 || $statusCode >= 300) {
+            throw new Exception($response->getBody());
+        }
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function recordCountInAModule(string $module_api_name, string $criteria)
+    {
+        $token = $this->generaAccessToken();
+
+        $response = $this->client->request('get', $this->config->url_crm_api . "/$module_api_name/actions/count", [
+            'headers' => [
+                'Authorization' => 'Zoho-oauthtoken ' . $token['access_token'],
+            ],
+            'query' => [
+                'criteria' => $criteria,
             ],
         ]);
 
