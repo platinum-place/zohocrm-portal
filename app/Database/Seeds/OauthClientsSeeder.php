b5dde377b40ecfb0bcb0980debba973e21456cfc
Warlyn <warlyn.garcia@bitboxonline.com>
2025-04-04 07:24:29 -0400

    Add CLI output for OauthClientsSeeder client credentials
    Separate client ID and secret into variables for reusability. Display the generated client ID and secret in the CLI to assist developers during seeding. This improves visibility and debugging during the setup process.

--- app/Database/Seeds/OauthClientsSeeder.php
+++ app/Database/Seeds/OauthClientsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace App\Database\Seeds;
 
+use CodeIgniter\CLI\CLI;
 use CodeIgniter\Database\Seeder;
 
 class OauthClientsSeeder extends Seeder
@@ -10,12 +11,18 @@ class OauthClientsSeeder extends Seeder
     {
         helper('string_util');
 
+        $client_id = generate_uuid_string();
+        $client_secret = bin2hex(random_bytes(16));
+
         $data = [
-            'client_id' => generate_uuid_string(),
-            'client_secret' => bin2hex(random_bytes(16)),
+            'client_id' => $client_id,
+            'client_secret' => $client_secret,
             'redirect_uri' => 'http://fake/',
         ];
 
         $this->db->table('oauth_clients')->insert($data);
+
+        CLI::write('Client ID: ' . $client_id, 'green');
+        CLI::write('Client Secret: ' . $client_secret, 'yellow');
     }
 }
