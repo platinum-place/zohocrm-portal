0a3e468c366eb641a3edb58ce7ff1d7f48249a51
warlyn <warlyn200@gmail.com>
2025-04-03 22:15:39 -0400

    **Generate UUIDs for OAuth clients and enhance security**
    Replaced hardcoded client ID and secret with dynamically generated values using a new helper function. Added `generate_uuid_string` in `string_util_helper` to create UUIDs and improved client secret generation with secure random bytes.

--- app/Database/Seeds/OauthClientsSeeder.php
+++ app/Database/Seeds/OauthClientsSeeder.php
@@ -8,10 +8,12 @@ class OauthClientsSeeder extends Seeder
 {
     public function run()
     {
+        helper('string_util');
+
         $data = [
-            'client_id'     => 'testclient',
-            'client_secret' => 'testpass',
-            'redirect_uri'  => 'http://fake/',
+            'client_id' => generate_uuid_string(),
+            'client_secret' => bin2hex(random_bytes(16)),
+            'redirect_uri' => 'http://fake/',
         ];
 
         $this->db->table('oauth_clients')->insert($data);
