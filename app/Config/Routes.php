2d00b3171e18c95cfb11525eddf391fc424e8c21
warlyn <warlyn200@gmail.com>
2025-05-02 01:01:53 -0400

    Fix file paths in OAuth migrations and enable App routes
    Corrected migration file paths to ensure proper database setup for OAuth tables. Additionally, re-enabled the inclusion of App routes in the routing configuration. This ensures both database migrations and routing work as intended.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-//require APPPATH . 'Routes/App.php';
+require APPPATH . 'Routes/App.php';
 require APPPATH . 'Routes/Api.php';
