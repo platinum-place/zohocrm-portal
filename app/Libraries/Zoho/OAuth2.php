9bedc83994e88d9bcdfef0daa6d1c03742165ded
warlyn <warlyn200@gmail.com>
2025-05-05 22:42:43 -0400

    laravel

--- app/Libraries/Zoho/OAuth2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Libraries\Zoho;
-
-class OAuth2
-{
-
-}
--- app/Views/components/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="layoutAuthentication_footer">
-    <footer class="py-4 bg-light mt-auto">
-        <div class="container-fluid px-4">
-            <div class="d-flex align-items-center justify-content-between small">
-                <div class="text-muted">Copyright &copy; Grupo Nobe <?= date('Y') ?></div>
-            </div>
-        </div>
-    </footer>
-</div>
