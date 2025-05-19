9fb9507b296a63cb9b0c6bff4d74a3466da9c406
Warlyn <warlyn.garcia@bitboxonline.com.do>
2025-05-19 15:45:13 -0400

    Add get and update methods to ZohoQuoteService
    Introduce `get` and `update` methods to handle Zoho Quotes using `getRecords` and `updateRecords` respectively. Added exception annotations to specify potential errors during API interactions.

--- app/Services/Zoho/ZohoQuoteService.php
+++ app/Services/Zoho/ZohoQuoteService.php
@@ -2,4 +2,31 @@
 
 namespace App\Services\Zoho;
 
-class ZohoQuoteService extends ZohoService {}
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
+use Throwable;
+
+class ZohoQuoteService extends ZohoService
+{
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function get(string $id): ?array
+    {
+        $fields = ['id'];
+
+        return $this->getRecords('Quotes', $fields, $id);
+    }
+
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function update(string $id, array $data): ?array
+    {
+        return $this->updateRecords('Quotes', $id, $data);
+    }
+}
