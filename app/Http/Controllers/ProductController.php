733cd8adbdf7c0532c1e5f11eb49ad0cb2699efb
warlyn <warlyn200@gmail.com>
2025-05-18 22:56:04 -0400

    Add ZohoProduct integration for fetching product data
    Integrate ZohoProduct service into ProductController to fetch product lists and details dynamically from Zoho API. This replaces the hardcoded responses with data retrieved via Zoho API, improving flexibility and scalability.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,23 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Zoho\ZohoProduct;
+use App\Services\Zoho\ZohoVehicle;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
+
 class ProductController extends Controller
 {
+    public function __construct(protected ZohoProduct $zohoProduct)
+    {
+    }
+
+    /**
+     * @throws RequestException
+     * @throws \Throwable
+     * @throws ConnectionException
+     */
     public function list()
     {
-        return response()->json([
-            '01' => 'Automovil',
-            '02' => 'Vida',
-        ]);
+        $list = $this->zohoProduct->getList();
+
+        return response()->json(
+            collect($list['data'])->map(fn($value) => [
+                $value['id'] => $value['Product_Category']
+            ])
+        );
     }
 
     public function show(string $id)
     {
+        $plan = $this->zohoProduct->get($id);
+
+        $productData = $plan['data'][0];
+
         return response()->json([
-            '01' => [
-                '01' => 'MAPFRE',
-                '02' => 'ANGLOAMERICANA',
-            ],
+            $productData['id'] => [
+                [
+                    $productData['Vendor_Name']['id'] => $productData['Product_Name']
+                ]
+            ]
         ]);
     }
 }
---
+++ app/Services/Zoho/ZohoProduct.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\Zoho;
+
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
+use Throwable;
+
+class ZohoProduct extends ZohoService
+{
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function getList(?int $page = 1, ?int $perPage = 200): ?array
+    {
+        $criteria = "Corredor:equals:" . env('ZOHO_ACCOUNT_ID');
+
+        return $this->searchRecords('Products', $criteria, $page, $perPage);
+    }
+
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function get(string $id): ?array
+    {
+        $fields = ['id', 'Vendor_Name','Product_Name'];
+
+        return $this->getRecords('Products', $fields,$id);
+    }
+}
