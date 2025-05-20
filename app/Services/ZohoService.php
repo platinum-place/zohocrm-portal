e629f24111a985ffc0d0ccc7b6b91d0538d10b79
warlyn <warlyn200@gmail.com>
2025-05-19 20:29:32 -0400

    Add ZohoService for managing Zoho access tokens
    Introduce ZohoService to handle access token creation, refresh, and retrieval for Zoho integration. Implements methods to generate, refresh, and validate tokens using ZohoOAuth API. This streamlines token management and ensures proper access control.

---
+++ app/Services/ZohoService.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Zoho\ZohoOauthAccessToken;
+use App\Models\Zoho\ZohoOauthRefreshToken;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
+use Throwable;
+use ZohoOAuth;
+
+abstract class ZohoService
+{
+    protected function createAccessToken(array $data): void
+    {
+        $expiresAt = now()->addSeconds($data['expires_in']);
+
+        ZohoOauthAccessToken::create([
+            'access_token' => $data['access_token'],
+            'expires_at' => $expiresAt,
+        ]);
+    }
+
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function generateAccessToken(string $grantToken): string
+    {
+        $response = ZohoOAuth::getPersistentToken($grantToken);
+
+        $this->createAccessToken($response);
+
+        ZohoOauthRefreshToken::create([
+            'refresh_token' => $response['refresh_token'],
+        ]);
+
+        return $response['access_token'];
+    }
+
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function refreshAccessToken(): string
+    {
+        $refreshToken = ZohoOauthRefreshToken::latest('id')->value('refresh_token');
+
+        if (! $refreshToken) {
+            throw new \Exception(__('Not Found'));
+        }
+
+        $response = ZohoOAuth::getTemporaryToken($refreshToken);
+
+        $this->createAccessToken($response);
+
+        return $response['access_token'];
+    }
+
+    /**
+     * @throws RequestException
+     * @throws Throwable
+     * @throws ConnectionException
+     */
+    public function getAccessToken(): string
+    {
+        $token = ZohoOauthAccessToken::latest('id')
+            ->where('expires_at', '>=', now())
+            ->value('access_token');
+
+        if (! $token) {
+            $token = $this->refreshAccessToken();
+        }
+
+        return $token;
+    }
+}
