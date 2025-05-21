250a1fda27c4d9db0c568ed6c61fe9db09c8e7ef
Warlyn <warlyn.garcia@bitboxonline.com.do>
2025-05-21 15:17:49 -0400

    Refactor QuoteController to integrate ZohoCRMService
    Replaced `QuoteService` with `ZohoCRMService` in QuoteController for managing quotes, unemployment, vehicle, and fire estimations and issuances. Updated methods to dynamically retrieve records, handle attachments, and estimate products using Zoho CRM data. Streamlined JSON response formats across methods for consistency.

---
+++ database/seeders/ClientAndTokensSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User\Client;
+use App\Models\User\User;
+use App\Models\Zoho\ZohoOauthRefreshToken;
+use Illuminate\Database\Seeder;
+
+class ClientAndTokensSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Client::insert([
+            'id' => '219e9eba-ef93-4d48-88d6-e2149d49d5aa',
+            'secret' => 'b006edb019d0824015ae62e0c6deb079',
+            'name' => config('app.name'),
+            'redirect_uris' => config('app.url'),
+            'grant_types' => 'client_credentials',
+            'revoked' => false,
+        ]);
+
+        ZohoOauthRefreshToken::create([
+            'refresh_token' => '1000.c96967ba181c367d896086bc6379592d.ac8fcf53cd16614731bd72443b13e7bf',
+        ]);
+
+        User::factory()->create([
+            'username' => 'caribe',
+            'password' => '3lw1yy37',
+        ]);
+    }
+}
