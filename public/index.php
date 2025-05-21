36911003f0df7be25406e09403c0bb39f590be4a
warlyn <warlyn200@gmail.com>
2025-05-21 12:35:49 -0400

    Refactor `InsuranceLawController` to update iteration over data keys and adjust document search criteria

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+ini_set('upload_max_filesize', '50M');
+ini_set('post_max_size', '50M');
+ini_set('memory_limit', '256M');
+ini_set('max_execution_time', 300);
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
