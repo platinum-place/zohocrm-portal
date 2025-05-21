42cb03d9c8a8a90884c466446342c75c8267cab7
warlyn <warlyn200@gmail.com>
2025-05-21 10:39:14 -0400

    Refactor `QuoteController` to remove unused imports, update formatting, and simplify methods

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Quote\CancelVehicleRequest;
-use App\Http\Requests\Quote\DisableVehicleLawRequest;
 use App\Http\Requests\Quote\EstimateFireRequest;
 use App\Http\Requests\Quote\EstimateUnemploymentDebtRequest;
-use App\Http\Requests\Quote\EstimateVehicleLawRequest;
 use App\Http\Requests\Quote\EstimateVehicleRequest;
 use App\Http\Requests\Quote\InspectRequest;
 use App\Http\Requests\Quote\IssueLifeRequest;
 use App\Http\Requests\Quote\IssueVehicleRequest;
-use App\Http\Requests\Quote\SearchDocumentRequest;
 use App\Http\Requests\Quote\ValidateInspectionRequest;
-use App\Services\Quote\QuoteService;
+use App\Services\QuoteService;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Storage;
@@ -22,7 +19,9 @@ use Throwable;
 
 class QuoteController extends Controller
 {
-    public function __construct(protected QuoteService $service) {}
+    public function __construct(protected QuoteService $service)
+    {
+    }
 
     public function estimateVehicle(EstimateVehicleRequest $request)
     {
@@ -104,7 +103,7 @@ class QuoteController extends Controller
                 'Coberturas' => $line['Product_Name']['id'],
                 'Quote_Stage' => 'Emitida',
                 'Vigencia_desde' => date('Y-m-d'),
-                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d').'+ 1 years')),
+                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d') . '+ 1 years')),
                 'Prima_neta' => round($line['Net_Total'] / 1.16, 2),
                 'ISC' => round($line['Net_Total'] - ($line['Net_Total'] / 1.16), 2),
                 'Prima' => round($line['Net_Total'], 2),
@@ -173,7 +172,7 @@ class QuoteController extends Controller
         ];
 
         foreach ($photos as $photo => $title) {
-            if (! $request->filled($photo)) {
+            if (!$request->filled($photo)) {
                 continue;
             }
 
@@ -189,7 +188,7 @@ class QuoteController extends Controller
                 default => throw new \Exception(__('validation.mimetypes', ['values' => '.jpg,.png']))
             };
 
-            $path = "photos/{$id}/uploads/".date('YmdHis')."/$title.$extension";
+            $path = "photos/{$id}/uploads/" . date('YmdHis') . "/$title.$extension";
 
             Storage::put($path, $imageData);
             $this->service->uploadAttachment($id, $path);
@@ -239,7 +238,7 @@ class QuoteController extends Controller
                 default => throw new \Exception(__('validation.mimetypes', ['values' => '.jpg,.png']))
             };
 
-            $path = "photos/{$id}/downloads/".date('YmdHis')."/{$attachment['File_Name']}.$extension";
+            $path = "photos/{$id}/downloads/" . date('YmdHis') . "/{$attachment['File_Name']}.$extension";
 
             Storage::put($path, $imageData);
 
@@ -321,14 +320,14 @@ class QuoteController extends Controller
     {
         $id = $request->get('Identificador');
 
-        $quote = $this->service->get($id)['data'][0];;
+        $quote = $this->service->get($id)['data'][0];
 
         foreach ($quote['Quoted_Items'] as $line) {
             $data = [
                 'Coberturas' => $line['Product_Name']['id'],
                 'Quote_Stage' => 'Emitida',
                 'Vigencia_desde' => date('Y-m-d'),
-                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d').'+ 1 years')),
+                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d') . '+ 1 years')),
                 'Prima_neta' => round($line['Net_Total'] / 1.16, 2),
                 'ISC' => round($line['Net_Total'] - ($line['Net_Total'] / 1.16), 2),
                 'Prima' => round($line['Net_Total'], 2),
@@ -428,7 +427,7 @@ class QuoteController extends Controller
                 'Coberturas' => $line['Product_Name']['id'],
                 'Quote_Stage' => 'Emitida',
                 'Vigencia_desde' => date('Y-m-d'),
-                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d').'+ 1 years')),
+                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d') . '+ 1 years')),
                 'Prima_neta' => round($line['Net_Total'] / 1.16, 2),
                 'ISC' => round($line['Net_Total'] - ($line['Net_Total'] / 1.16), 2),
                 'Prima' => round($line['Net_Total'], 2),
@@ -538,156 +537,111 @@ class QuoteController extends Controller
         return response()->noContent();
     }
 
-    public function estimateVehicleLaw(EstimateVehicleLawRequest $request)
-    {
-        return response()->json([
-            [
-                'Passcode' => '4821',
-                'OfertaID' => 105,
-                'Prima' => 12500.75,
-                'Impuesto' => 1875.11,
-                'PrimaTotal' => 14375.86,
-                'PrimaCuota' => 1197.99,
-                'Planid' => 3,
-                'Plan' => 'Plan Básico',
-                'Aseguradora' => 'Seguros XYZ',
-                'Idcotizacion' => 3222373000214281001,
-                'Fecha' => now()->toDateTimeString(),
-                'CoberturasList' => [
-                    [
-                        'id' => 1,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                    [
-                        'id' => 2,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                    [
-                        'id' => 3,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                ],
-            ],
-            [
-                'Passcode' => '4821',
-                'OfertaID' => 105,
-                'Prima' => 12500.75,
-                'Impuesto' => 1875.11,
-                'PrimaTotal' => 14375.86,
-                'PrimaCuota' => 1197.99,
-                'Planid' => 3,
-                'Plan' => 'Plan Básico',
-                'Aseguradora' => 'Seguros XYZ',
-                'Idcotizacion' => 3222373000214281001,
-                'Fecha' => now()->toDateTimeString(),
-                'CoberturasList' => [
-                    [
-                        'id' => 1,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                    [
-                        'id' => 2,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                    [
-                        'id' => 3,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                ],
-            ],
-            [
-                'Passcode' => '4821',
-                'OfertaID' => 105,
-                'Prima' => 12500.75,
-                'Impuesto' => 1875.11,
-                'PrimaTotal' => 14375.86,
-                'PrimaCuota' => 1197.99,
-                'Planid' => 3,
-                'Plan' => 'Plan Básico',
-                'Aseguradora' => 'Seguros XYZ',
-                'Idcotizacion' => 3222373000214281001,
-                'Fecha' => now()->toDateTimeString(),
-                'CoberturasList' => [
-                    [
-                        'id' => 1,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                    [
-                        'id' => 2,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                    [
-                        'id' => 3,
-                        'nombre' => 'Cobertura Total',
-                        'descripcion' => 'Cobertura completa del vehículo',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function paymentType()
-    {
-        return response()->json([
-            [
-                'ID' => 1,
-                'TIPOVEHICULOID' => 1.0,
-                'TIPOVEHICULO' => 'Motocicletas hasta 250 CC',
-                'COBERTURADPAYRC' => '50/50/100',
-                'FINANZAJUDICIAL' => '50000',
-            ],
-        ]);
-    }
 
     /**
      * @throws RequestException
      * @throws Throwable
      * @throws ConnectionException
      */
-    public function searchDocument(SearchDocumentRequest $request, string $identification)
+    public function estimateLife(EstimateLifeRequest $request)
     {
-        //        $search = $request->get('NoDocumento');
-
-        $quotes = $this->service->searchQuote($identification);
+        $products = $this->service->getLifeProducts('Vida');
 
         $response = [];
 
-        foreach ($quotes as $quote) {
+        foreach ($products as $product) {
+            $alert = '';
+
+            if ($request->get('PlazoDias') > $product['Plazo_max']) {
+                $alert = 'El plazo es mayor al limite establecido.';
+            }
+
+            if ($request->get('MontoOriginal') < $product['Suma_asegurada_min'] || $request->get('MontoOriginal') > $product['Suma_asegurada_max']) {
+                $alert = 'El plazo es mayor al limite establecido.';
+            }
+
+            $debtTax = 0;
+            $coDebtTax = 0;
+            $amount = 0;
+
+            try {
+                $taxes = $this->service->getProductTaxes($product['id']);
+
+                foreach ($taxes as $tax) {
+                    if ($request->get('Edad') >= $tax['Edad_min'] and $request->get('Edad') <= $tax['Edad_max']) {
+                        $debtTax = $tax['Name'] / 100;
+                    } else {
+                        $alert = 'La edad del deudor no estan dentro del limite permitido.';
+                    }
+
+                    if ($request->get('codeudor') and $request->get('EdadCodeudor')) {
+                        if ($request->get('EdadCodeudor') >= $tax['Edad_min'] and $request->get('EdadCodeudor') <= $tax['Edad_max']) {
+                            $coDebtTax = $tax['Codeudor'] / 100;
+                        } else {
+                            $alert = 'La edad del codeudor no estan dentro del limite permitido.';
+                        }
+                    }
+                }
+            } catch (Throwable $throwable) {
+
+            }
+
+            if (empty($alert)) {
+                if ($debtTax && $coDebtTax) {
+                    $amountDebt = ($request->get('MontoOriginal') / 1000) * $debtTax;
+                    $amountCoDebt = ($request->get('MontoOriginal') / 1000) * ($coDebtTax - $debtTax);
+                    $amount = $amountDebt + $amountCoDebt;
+                } elseif ($debtTax) {
+                    $amount = ($request->get('MontoOriginal') / 1000) * $debtTax;
+                }
+
+                $amount = round($amount, 2);
+            }
+
+            $data = [
+                'Subject' => $request->get('NombreCliente'),
+                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d') . '+ 30 days')),
+                'Vigencia_desde' => date('Y-m-d'),
+                'Account_Name' => 3222373000092390001,
+                'Contact_Name' => 3222373000203318001,
+                'Quote_Stage' => 'Cotizando',
+                'Nombre' => $request->get('NombreCliente'),
+                'RNC_C_dula' => $request->get('IdenCliente'),
+                'Direcci_n' => $request->get('Direccion'),
+                'Tel_Celular' => $request->get('Telefono1'),
+                'Plan' => 'Vida',
+                'Suma_asegurada' => $request->get('MontoOriginal'),
+                'Plazo' => $request->get('PlazoAnios'),
+                'Fuente' => 'API',
+                'Quoted_Items' => [
+                    [
+                        'Quantity' => 1,
+                        'Product_Name' => $product['id'],
+                        'Total' => $amount,
+                        'Net_Total' => $amount,
+                        'List_Price' => $amount,
+                    ],
+                ],
+            ];
+
+            $responseProduct = $this->service->create($data);
+
             $response[] = [
-                'IDCliente' => $identification,
-                'NombreCliente' => $quote['Nombre'],
-                'Direccion' => $quote['Direcci_n'],
-                'Telefono' => $quote['Tel_Residencia'],
-                'IDTipoVehiculo' => null,
-                'TipoVehiculo' => $quote['Tipo_veh_culo'],
-                'Marca' => $quote['Marca']['name'],
-                'Modelo' => $quote['Modelo']['name'],
-                'Correo' => $quote['Correo_electr_nico'],
-                'Anio' => $quote['A_o'],
-                'Color' => $quote['Color'],
-                'Chassis' => $quote['Chasis'],
-                'Placa' => $quote['Placa'],
-                'Poliza' => null,
-                'Prima' => $quote['Created_Time'],
-                'Vigencia' => '12 Meses',
-                'Cobertura' => null,
-                'FianzaJudicial' => null,
-                'FechaEmision' => $quote['Created_Time'],
-                'Estado' => $quote['Quote_Stage'] === 'Cancelada' ? 0 : 1,
-                'Usuario' => null,
-                'PDV' => null,
-                'C3_Meses' => null,
-                'C6_Meses' => null,
-                'C12_Meses' => null,
-                'Error' => null,
+                'Impuesto' => null,
+                'PrimaPeriodo' => null,
+                'PrimaTotal' => $amount,
+                'identificador' => $responseProduct['details']['id'],
+                'Aseguradora' => $product['Vendor_Name']['name'],
+                'MontoOrig' => $request->get('MontoOriginal'),
+                'Anios' => null,
+                'EdadTerminar' => null,
+                'Cliente' => $request->get('NombreCliente'),
+                'Fecha' => date('d/m/Y'),
+                'Direccion' => $request->get('Direccion'),
+                'Edad' => $request->get('Edad'),
+                'IdenCliente' => $request->get('IdenCliente'),
+                'Codeudor' => null,
+                'Alerta' => $alert,
             ];
         }
 
@@ -695,19 +649,84 @@ class QuoteController extends Controller
     }
 
     /**
-     * @throws RequestException
      * @throws Throwable
      * @throws ConnectionException
+     * @throws RequestException
      */
-    public function disableVehicleLaw(DisableVehicleLawRequest $request, string $id)
+    public function issueLife(IssueLifeRequest $request)
     {
-        $this->service->get($id)['data'][0];
+        $id = $request->get('Identificador');
 
-        $data = [
-            'Quote_Stage' => 'Cancelada',
-        ];
+        $quote = $this->service->get($id)['data'][0];
 
-        $this->service->update($id, $data);
+        foreach ($quote['Quoted_Items'] as $line) {
+            $data = [
+                'Coberturas' => $line['Product_Name']['id'],
+                'Quote_Stage' => 'Emitida',
+                'Vigencia_desde' => date('Y-m-d'),
+                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d') . '+ 1 years')),
+                'Prima_neta' => round($line['Net_Total'] / 1.16, 2),
+                'ISC' => round($line['Net_Total'] - ($line['Net_Total'] / 1.16), 2),
+                'Prima' => round($line['Net_Total'], 2),
+            ];
+
+            $this->service->update($id, $data);
+
+            break;
+        }
+
+        return response()->noContent();
+    }
+
+    public function estimateUnemployment(EstimateUnemploymentRequest $request)
+    {
+        $products = $this->service->getLifeProducts('Desempleo');
+
+        $response = [];
+
+        foreach ($products as $product) {
+            $response[] = [
+                'Impuesto' => '18.5',
+                'PrimaPeriodo' => '000.00',
+                'PrimaTotal' => '000.00',
+                'identificador' => '3222373000214281001',
+                'Cliente' => 'Fulano de Tal',
+                'Direccion' => 'Calle Primera',
+                'Fecha' => '01/01/2020',
+                'TipoEmpleado' => 'Privado',
+                'IdentCliente' => '00030489834989',
+                'Aseguradora ' => 'Mapfre',
+                'MontoOriginal' => '000.00',
+                'Cuota' => '000.00',
+                'PlazoMese' => '24',
+                'Total' => '000.00',
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+    public function issueUnemployment(IssueLifeRequest $request)
+    {
+        $id = $request->get('Identificador');
+
+        $quote = $this->service->get($id)['data'][0];
+
+        foreach ($quote['Quoted_Items'] as $line) {
+            $data = [
+                'Coberturas' => $line['Product_Name']['id'],
+                'Quote_Stage' => 'Emitida',
+                'Vigencia_desde' => date('Y-m-d'),
+                'Valid_Till' => date('Y-m-d', strtotime(date('Y-m-d') . '+ 1 years')),
+                'Prima_neta' => round($line['Net_Total'] / 1.16, 2),
+                'ISC' => round($line['Net_Total'] - ($line['Net_Total'] / 1.16), 2),
+                'Prima' => round($line['Net_Total'], 2),
+            ];
+
+            $this->service->update($id, $data);
+
+            break;
+        }
 
         return response()->noContent();
     }
