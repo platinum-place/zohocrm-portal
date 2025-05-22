9805442d906c3cb20caf4319089c5b65e180eea0
warlyn <warlyn200@gmail.com>
2025-05-22 17:14:49 -0400

    Remove all database and Docker configuration files.
    This commit deletes testing database setup scripts, Dockerfiles, and associated configuration files for PHP versions 8.0 to 8.4. These files were removed to streamline the repository or due to deprecation or redundancy in the current setup.

--- database/seeders/ClientAndTokensSeeder.php
+++ database/seeders/ClientAndTokensSeeder.php
@@ -22,6 +22,7 @@ class ClientAndTokensSeeder extends Seeder
         //            'grant_types' => 'client_credentials',
         //            'revoked' => false,
         //        ]);
+//        Client::firstWhere('id', '0196f5f9-1eda-7248-999d-b6a84a4043fa')->update(['id' => '219e9eba-ef93-4d48-88d6-e2149d49d5aa','secret' => 'b006edb019d0824015ae62e0c6deb079']);
 
         ZohoOauthRefreshToken::create([
             'refresh_token' => '1000.c96967ba181c367d896086bc6379592d.ac8fcf53cd16614731bd72443b13e7bf',
