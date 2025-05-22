3677ae1fa5438fc8203ae21b5f27b2f46fde0a16
warlyn <warlyn200@gmail.com>
2025-05-21 22:16:23 -0400

    Fix indentation in commented-out Client seeding code
    Adjusted the indentation of the commented-out Client::insert block for better code readability and consistency. No functional changes were made.

--- database/seeders/ClientAndTokensSeeder.php
+++ database/seeders/ClientAndTokensSeeder.php
@@ -14,14 +14,14 @@ class ClientAndTokensSeeder extends Seeder
      */
     public function run(): void
     {
-//        Client::insert([
-//            'id' => '219e9eba-ef93-4d48-88d6-e2149d49d5aa',
-//            'secret' => 'b006edb019d0824015ae62e0c6deb079',
-//            'name' => config('app.name'),
-//            'redirect_uris' => '',
-//            'grant_types' => 'client_credentials',
-//            'revoked' => false,
-//        ]);
+        //        Client::insert([
+        //            'id' => '219e9eba-ef93-4d48-88d6-e2149d49d5aa',
+        //            'secret' => 'b006edb019d0824015ae62e0c6deb079',
+        //            'name' => config('app.name'),
+        //            'redirect_uris' => '',
+        //            'grant_types' => 'client_credentials',
+        //            'revoked' => false,
+        //        ]);
 
         ZohoOauthRefreshToken::create([
             'refresh_token' => '1000.c96967ba181c367d896086bc6379592d.ac8fcf53cd16614731bd72443b13e7bf',
