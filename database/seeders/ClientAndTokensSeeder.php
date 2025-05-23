61f8bf6e35655aafeb11e7939ba7aa47b6b1c2ac
Warlyn <warlyn.garcia@bitboxonline.com.do>
2025-05-23 10:57:31 -0400

    Add missing Spanish translations for Zoho error messages
    Added translations for "Record not found in Zoho" and "Records not found in Zoho" to the Spanish language file. This ensures error messages are properly localized for Spanish-speaking users.

--- database/seeders/ClientAndTokensSeeder.php
+++ database/seeders/ClientAndTokensSeeder.php
@@ -22,7 +22,10 @@ class ClientAndTokensSeeder extends Seeder
         //            'grant_types' => 'client_credentials',
         //            'revoked' => false,
         //        ]);
-        //        Client::firstWhere('id', '0196f9fd-bebf-70b6-9add-95bcdda4119d')->update(['id' => '219e9eba-ef93-4d48-88d6-e2149d49d5aa','secret' => 'b006edb019d0824015ae62e0c6deb079']);
+        //        Client::firstWhere('id', '0196fda4-6071-7138-a86f-b5226667a930')->update([
+        //            'id' => '219e9eba-ef93-4d48-88d6-e2149d49d5aa',
+        //            'secret' => 'b006edb019d0824015ae62e0c6deb079'
+        //        ]);
 
         ZohoOauthRefreshToken::create([
             'refresh_token' => '1000.c96967ba181c367d896086bc6379592d.ac8fcf53cd16614731bd72443b13e7bf',
